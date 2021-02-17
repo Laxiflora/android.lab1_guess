a6b59525cb8eba0fca6c85904e8dd84e3dd1b6b2
laxiflora <liu200113@gmail.com>
2021-02-17 18:04:46 +0800

    Android 3-5 finish

--- app/src/main/java/com/raccoon/lab1_guess/MainActivity.kt
+++ app/src/main/java/com/raccoon/lab1_guess/MainActivity.kt
@@ -3,12 +3,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {   //同C++,繼承AppCompatActivity
+    val secret = SecretNumber()
     override fun onCreate(savedInstanceState: Bundle?) {  //複寫父類別的function
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)  //R=package name, get res/layout/activity_main
+        Log.d("MainActivity","偷偷作弊:答案是"+secret.secretNum)
         //R.id.hello    給定layout中的id，會回傳一個屬於他的ID(int)
         //R.drawable.xxx 抓取res/drawable/xxx
     }
@@ -17,6 +21,23 @@ class MainActivity : AppCompatActivity() {   //同C++,繼承AppCompatActivity
         val playerInput = inputNumber.text.toString().toInt()
        // println("number : $playerInput")
         Log.d("MainActivity","number: "+playerInput)  //d = debug, debug訊息在上架的時候會自動被隱藏
+        if(secret.validate(playerInput)==0){
+        //    Toast.makeText(this,"恭喜答對",Toast.LENGTH_LONG).show()
+            AlertDialog.Builder(this)
+                    .setTitle("結果")
+                    .setMessage("恭喜答對")
+                    .setPositiveButton("OK",null)
+                    .show()
+            secret.right()
+        }
+        else{
+            AlertDialog.Builder(this)
+                    .setTitle("結果")
+                    .setMessage("答錯囉，你已經猜了${secret.count}次")
+                    .setPositiveButton("OK",null)
+                    .show()
+        //    Toast.makeText(this,"答錯囉，你已經猜了${secret.count}次",Toast.LENGTH_LONG).show()
+        }
     }
 
 }
--- app/src/main/java/com/raccoon/lab1_guess/SecretNumber.kt
+++ app/src/main/java/com/raccoon/lab1_guess/SecretNumber.kt
@@ -2,16 +2,21 @@ package com.raccoon.lab1_guess
 import java.util.*
 
 class SecretNumber {
-    val secret : Int = Random().nextInt(10)+1
-    val count : Int = 0
+    var secretNum : Int = Random().nextInt(10)+1
+    var count : Int = 0
 
     fun validate(number : Int) : Int{
-        return number - secret
+        count=count+1
+        return number - secretNum
+    }
+    fun right(){
+      //  secretNum = Random().nextInt(10)+1
+        count=0
     }
 }
 
 fun main(){
     val secretNumber = SecretNumber()
-    println(secretNumber.secret)
+    println(secretNumber.secretNum)
 
 }
